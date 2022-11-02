812208f27b0bf895cd35041d1b9e1c78852ec33c
Juho Lindemark <Juho.Lindemark@vuoksi.fi>
2022-11-02 15:59:22 +0200

    Added player audio player...KEKW

---
+++ Assets/Scripts/Audio/CharacterAudio.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAudio : MonoBehaviour
+{
+    public enum Sounds
+    {
+        walk,
+        melee,
+        jump,
+        range
+    }
+
+    public enum SoundModifier
+    {
+        play,
+        stop
+    }
+
+    [SerializeField] AudioSource footstepSource;
+
+    [SerializeField] AudioSource meleeSource;
+
+    [SerializeField] AudioSource jumpSource;
+
+    [SerializeField] AudioSource rangedSource;
+
+
+    public void ModifySound(Sounds sound, SoundModifier soundModifier, bool oneShot = true)
+    {
+        AudioSource x = GetSource(sound);
+
+        switch (soundModifier)
+        {
+            case SoundModifier.play:
+                if(!oneShot)
+                {
+                    x.Stop();
+                    x.Play();
+                }
+                else
+                {
+                    x.PlayOneShot(x.clip);
+                }
+                break;
+            case SoundModifier.stop:
+                x.Stop();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private AudioSource GetSource(Sounds sound)
+    {
+        switch (sound)
+        {
+            case Sounds.walk:
+                return footstepSource;
+            case Sounds.melee:
+                return meleeSource;
+            case Sounds.jump:
+                return jumpSource;
+            case Sounds.range:
+                return rangedSource;
+            default:
+                return null;
+        }
+    }
+}
