f04be1ce185b3236fd94b1bcb934f7de3fcc2761
Juho Lindemark <Juho.Lindemark@vuoksi.fi>
2022-11-02 10:00:56 +0200

    Added base enemy and ranged enemy

--- Assets/Scripts/Controller/AiController.cs
+++ Assets/Scripts/Controller/AiController.cs
@@ -36,8 +36,32 @@ namespace Footkin.Controller
                 }
                 followPlayer = false;
             }
-            // Todo raycast
-            attack.OnMeleeAttack();
+
+            //Debug.DrawRay(this.transform.position, -transform.GetChild(0).transform.right * 2f, Color.red, 1f);
+            // hit detection for melee attack key press
+            if (Physics.Raycast(this.transform.position, -transform.GetChild(0).transform.right, out RaycastHit hit, 2f, 1 << 6))
+            {
+                if (hit.transform.gameObject.CompareTag("Player"))
+                {
+                    if (attack.meleeWeapon)
+                    {
+                        attack.OnMeleeAttack();
+                    }
+                }
+            }
+
+            Debug.DrawRay(this.transform.position, -transform.GetChild(0).transform.right * 8f, Color.red, 1f);
+            // hit detection for ranged attack key press
+            if (Physics.Raycast(this.transform.position, -transform.GetChild(0).transform.right, out RaycastHit rangedhit, 8f, 1 << 6))
+            {
+                if (rangedhit.transform.gameObject.CompareTag("Player"))
+                {
+                    if (attack.rangedWeapon)
+                    {
+                        attack.OnRangedAttack();
+                    }
+                }
+            }
         }
 
         public void SetPlayerDetected(Vector3 detectedPosition)
